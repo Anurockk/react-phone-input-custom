64f0722c74f617250ad88cb56ffee821d2c0eccf
Anurag Singh <anuragsingh.carvia@gmail.com>
2025-02-24 19:01:58 +0530

    bug fix

--- src/Index.ts
+++ src/Index.ts
@@ -1,3 +1,2 @@
-export * from "./interface/PhoneInputProps";
 export { default as PhoneInput } from "./components/phoneInput/PhoneInput";
 
---
+++ src/types/react-phone-input-custom.d.ts
@@ -0,0 +1,26 @@
+declare module 'react-phone-input-custom' {
+    import { ComponentType } from 'react';
+
+    export interface PhoneInputProps {
+        fetchAllCountries?: boolean;
+        initialCountryCode?: string;
+        enableRedBorderOnError?: boolean;
+        maxPhoneLength?: number;
+        isPhoneNumberValid?: (isValid: boolean) => void;
+        containerStyle?: React.CSSProperties;
+        inputStyle?: React.CSSProperties;
+        dropdownStyle?: React.CSSProperties;
+        inputRef?: React.Ref<HTMLInputElement>;
+        onPhoneChange?: (data: {
+            dialCode: string;
+            countryCode: string;
+            nationalFormattedNumber: string;
+            internationalFormattedNumber: string;
+            e164Number: string;
+            mobileNumber: string;
+        }) => void;
+    }
+
+    const PhoneInput: ComponentType<PhoneInputProps>;
+    export default PhoneInput;
+}
