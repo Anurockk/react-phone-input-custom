1dd3787fee9925bbd68fe2b6b263752dabf6c9f5
Anurag Singh <anuragsingh.carvia@gmail.com>
2025-02-24 18:27:07 +0530

    bug fixes

--- src/Index.ts
+++ src/Index.ts
@@ -1,1 +1,1 @@
-export { default as PhoneInput } from './components/phoneInput/PhoneInput';
+export { default as PhoneInput } from "./components/phoneInput/PhoneInput";
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import * as path from "node:path";
 
 export default defineConfig({
-  plugins: [
-      react(),
-      tailwindcss(),
-  ],
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/index.ts"),
+            name: "ReactPhoneInputCustom",
+            fileName: (format) => `index.${format}.js`,
+            formats: ["es", "cjs", "umd"],
+        },
+        rollupOptions: {
+            external: ["react", "react-dom"],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                },
+            },
+        },
+    },
 });
